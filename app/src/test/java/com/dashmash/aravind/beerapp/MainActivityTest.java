e771fad900c131c4c23ed7aca5816b2d222b45ca
Aravindhan <virataravindhan@gmail.com>
2018-07-17 10:50:08 +0530

    source code

---
+++ app/src/test/java/com/dashmash/aravind/beerapp/MainActivityTest.java
@@ -0,0 +1,20 @@
+package com.dashmash.aravind.beerapp;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    public void testLaunch(){
+
+    }
+    @After
+    public void tearDown() throws Exception {
+    }
+}
